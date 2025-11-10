6ea80b7d20eb769b1aa10919b4fe9ec3c6169fea
Bl4z3uwu <gabrielalmeidajordao2030@gmail.com>
2025-11-10 15:55:58 -0300

    eu desisto de dar nome

--- listagem-midia-cards.php
+++ listagem-midia-cards.php
@@ -4,14 +4,51 @@ session_start();
 require 'logica-autenticacao.php';
 
 $titulo_pagina = 'Mídias';
+$ordem = "titulo";
 require 'cabecalho.php';
 
 require 'conexao.php';
 
-$sql = "SELECT id, titulo, ano, genero, poster FROM midias ORDER BY titulo";
+// Ordem
+if (!empty($_GET["campo"]) && !empty($_GET["ordem"])) {
+    // Whitelist
+    $valid_orders = ['id asc', 'id desc', 'titulo asc', 'titulo desc', 'ano asc', 'ano desc', 'genero asc', 'genero desc'];
+
+    if (isset($ordem) && in_array($_GET["campo"] . " " . $_GET["ordem"], $valid_orders)) {
+        $ordem = filter_input(INPUT_GET, 'ordem', FILTER_SANITIZE_SPECIAL_CHARS);
+        $campo = filter_input(INPUT_GET, 'campo', FILTER_SANITIZE_SPECIAL_CHARS);
+    }
+    else {
+        $ordem = 'titulo';
+    }
+}
+
+$sql = "SELECT id, titulo, ano, genero, poster FROM midias ORDER BY $campo $ordem";
+echo $sql;
 $stmt = $conn->query($sql);
 
 ?>
+
+<div class="d-flex justify-content-end me-4 my-3 ordenacao align-items-center">
+    <span class="me-2 fw-semibold">Ordenar por:</span>
+    <a href="?campo=id&ordem=<?php echo ($ordem == 'desc') ? 'asc' : 'desc' ?>" class="btn btn-outline-primary btn-sm mx-1 <?php echo ($campo == 'id') ? 'active' : '' ?>">
+        ID
+        <span data-feather="<?php echo ($ordem == 'desc') ? 'chevron-down' : 'chevron-up' ?>"></span>
+    </a>
+    <a href="?campo=titulo&ordem=<?php echo ($ordem == 'desc') ? 'asc' : 'desc' ?>" class="btn btn-outline-primary btn-sm mx-1 <?php echo ($campo == 'titulo') ? 'active' : '' ?>">
+        Título
+        <span data-feather="<?php echo ($ordem == 'desc') ? 'chevron-down' : 'chevron-up' ?>"></span>
+    </a>
+    <a href="?campo=genero&ordem=<?php echo ($ordem == 'desc') ? 'asc' : 'desc' ?>" class="btn btn-outline-primary btn-sm mx-1 <?php echo ($campo == 'genero') ? 'active' : '' ?>">
+        Gênero
+        <span data-feather="<?php echo ($ordem == 'desc') ? 'chevron-down' : 'chevron-up' ?>"></span>
+    </a>
+    <a href="?campo=ano&ordem=<?php echo ($ordem == 'desc') ? 'asc' : 'desc' ?>" class="btn btn-outline-primary btn-sm mx-1 <?php echo ($campo == 'ano') ? 'active' : '' ?>">
+        Ano
+        <span data-feather="<?php echo ($ordem == 'desc') ? 'chevron-down' : 'chevron-up' ?>"></span>
+    </a>
+</div>
+
 <div class="album py-5 gb-light">
     <div class="container">
             <?php
--- listagem-midia.php
+++ listagem-midia.php
@@ -8,19 +8,120 @@ require 'cabecalho.php';
 
 require 'conexao.php';
 
-$sql = "SELECT id, titulo, ano, genero, poster FROM midias ORDER BY titulo";
+$exibir_busca = false;
+$sql_busca = "";
+$busca = "";
+$tipo_busca = "";
+$ordem = "titulo";
+
+// Busca
+if (!empty($_POST["busca"])) {
+    $busca = filter_input(INPUT_POST, 'busca', FILTER_SANITIZE_SPECIAL_CHARS);
+    $tipo_busca = filter_input(INPUT_POST, 'tipo_busca', FILTER_SANITIZE_SPECIAL_CHARS);
+
+    $exibir_busca = true;
+
+    switch ($tipo_busca) {
+        case 'id':
+            $idBusca = intval($busca);
+            $sql_busca = " WHERE id LIKE '%$busca%' ";
+
+            break;
+        case 'genero':
+            $sql_busca = " WHERE genero LIKE '%$busca%' ";
+
+            break;
+        case 'titulo':
+            $sql_busca = " WHERE titulo LIKE '%$busca%' ";
+
+            break;
+        default:
+            $idBusca = intval($busca);
+            $sql_busca = " WHERE titulo LIKE '%$busca%' OR genero LIKE '%$busca%' OR id LIKE '%$idBusca%' ";
+            break;
+    }
+}
+// Ordem
+if (!empty($_GET["ordem"])) {
+    // Whitelist
+    $valid_orders = ['id', 'id desc', 'titulo', 'titulo desc', 'ano', 'ano desc'];
+
+    if (isset($ordem) && in_array($_GET["ordem"], $valid_orders)) {
+        $ordem = filter_input(INPUT_GET, 'ordem', FILTER_SANITIZE_SPECIAL_CHARS);
+    }
+    else {
+        $ordem = 'titulo';
+    }
+}
+
+$sql = "SELECT id, titulo, ano, genero, poster FROM midias $sql_busca ORDER BY $ordem";
 $stmt = $conn->query($sql);
 
 echo "<p>Driver: <b>" . $conf["driver"] . "</b> </p>";
 
 ?>
+
+<div class="row mb-3">
+    <form action="" method="POST" class="row">
+        <label class="col-sm-2 col-form-label text-end">
+            Buscar por
+        </label>
+        <div class="col-sm-2">
+            <select name="tipo_busca" id="tipo_busca" class="form-select">
+                <option value="" <?= $tipo_busca == "" ? "selected" : "" ?>>Todos os campos</option>
+                <option value="id" <?= $tipo_busca == "id" ? "selected" : "" ?>>ID</option>
+                <option value="titulo" <?= $tipo_busca == "titulo" ? "selected" : "" ?>>Título</option>
+                <option value="genero" <?= $tipo_busca == "genero" ? "selected" : "" ?>>Gênero</option>
+            </select>
+        </div>
+        <div class="col-sm-6">
+            <input type="search" name="busca" id="busca" 
+                placeholder="Digite um termo" class="form-control" value="<?= $busca ?>">
+        </div>
+        <div class="col-sm-2">
+            <button class="btn btn-primary">
+                <i data-feather="search"></i> Pesquisar
+            </button>
+        </div>
+    </form>
+</div>
+<?php
+if ($exibir_busca) {
+?>
+<div class="alert alert-secondary">
+    Resultados para: <b><?= $busca ?></b>
+    - <a href="listagem-midia.php?ordem=<?=$ordem?>">Limpar</a>
+</div>
+<?php
+}
+
+if ($stmt->rowCount() == 0) {
+?>
+    <div class="alert alert-warning mt-3">
+        Nenhum resultado encontrado!
+    </div>
+<?php
+} else {
+?>
 <div class="table-responsive">
     <table class="table table-striped">
         <thead class="table-success">
             <tr>
-                <th scope="col" style="width: 10%;">ID</th>
-                <th scope="col" style="width: 25%;">Titulo</th>
-                <th scope="col" style="width: 10%;">Ano</th>
+                <th scope="col" style="width: 10%;">
+                    <a href="?ordem=<?= ($ordem == "id") ? "id desc" : "id"; ?>">ID</a>
+                    <?php if ($ordem == "id") echo "▼"; ?>
+                    <?php if ($ordem == "id desc") echo "▲"; ?>
+                </th>
+                <th scope="col" style="width: 25%;">
+                    <a href="?ordem=<?= ($ordem == "titulo") ? "titulo desc" : "titulo"; ?>">Titulo</a>
+                    <?php if ($ordem == "titulo") echo "▼"; ?>
+                    <?php if ($ordem == "titulo desc") echo "▲"; ?>
+                </th>
+                <th scope="col" style="width: 10%;">
+                    <a href="?ordem=<?= ($ordem == "ano") ? "ano desc" : "ano"; ?>">Ano</a>
+                    <?php if ($ordem == "ano") echo "▼"; ?>
+                    <?php if ($ordem == "ano desc") echo "▲"; ?>
+                </th>
                 <th scope="col" style="width: 25%;">Gênero</th>
                 <th scope="col" style="width: 15%;">Imagem</th>
                 <?php
@@ -75,7 +176,7 @@ echo "<p>Driver: <b>" . $conf["driver"] . "</b> </p>";
     </table>
 </div>
 <?php
-
-    require 'rodape.php';
+}
+require 'rodape.php';
 
 ?>
